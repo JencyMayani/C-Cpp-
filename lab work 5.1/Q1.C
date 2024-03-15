36ba2313413c7ab2a1048487db51c1f3fb46f4c6
JencyMayani <mayanijency16@gmail.com>
2024-03-15 21:18:48 +0530

    15/03/2024

---
+++ lab work 5.1/Q1.C
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int a,b;
+	clrscr();
+	printf("Enter First Number:%d",a);
+	scanf("%d", &a);
+	printf("Enter Second Number:%d",b);
+	scanf("%d", &b);
+	if(a<b)
+	{
+		printf("The Minimum Value:%d\n",a);
+	}
+	else
+	{
+		printf("The Minimum Value:%d\n",b);
+	}
+	getch();
+}
+
+
+
+
+
+
+
+
+
+
+
