5c706cce2fca276b7097834ac0a345a127fc7886
JencyMayani <mayanijency16@gmail.com>
2024-03-30 18:08:37 +0530

    30/03/2024

---
+++ lab work 6.3/Q1.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int i,n,sum=0;
+	clrscr();
+	printf("Enter Any Number:\n",n);
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		sum+=i;
+
+	}
+	printf("sum=%d\n",i);
+
+	getch();
+
+
+
+
+
+
+}
---
+++ lab work 6.3/Q2.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int i,n=1;
+	clrscr();
+	printf("Enter Any Number:\n",n);
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		n*=i;
+
+	}
+	printf("factorial=%d\n",i);
+
+	getch();
+
+
+
+
+
+
+}
